5d2746435f74aee8eef811871f29ead45184f182
rivka2211 <r0548500974@gmail.com>
2025-03-27 02:19:54 +0200

    chage Auth route

--- DocuManager/DockManeger.API/Controllers/AuthController.cs
+++ DocuManager/DockManeger.API/Controllers/AuthController.cs
@@ -9,7 +9,7 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class AuthController : ControllerBase
     {
